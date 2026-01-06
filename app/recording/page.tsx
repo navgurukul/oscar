2476eb50d74f9074e5507aeb9448a6fe32ac37f9
Chhaya Verma <chhayav300@gmail.com>
2026-01-06 22:51:08 +0530

    Remove header section from RecordingPage component

--- app/recording/page.tsx
+++ app/recording/page.tsx
@@ -254,14 +254,6 @@ export default function RecordingPage() {
         </button>
       </div>
 
-      {/* Header */}
-      <div className="absolute top-6 flex items-center gap-2">
-        <svg className="w-6 h-6 text-purple-500" fill="currentColor" viewBox="0 0 20 20">
-          <path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" />
-        </svg>
-        <h1 className="text-2xl font-bold text-purple-500">OSCAR</h1>
-      </div>
-
       <div className="flex flex-col items-center gap-8 mt-20">
         {/* Tap to start / Timer */}
         {!isRecording ? (
